022a93d015b67712b3b0f85e36ae9b6f34e477f5
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 10:49:40 -0400

    fixed

--- todo-server/src/app.ts
+++ todo-server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import todosRouter from "./todos/routes.ts"
+import cors from "cors"
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use("api/todos", todosRouter)
 
 app.get("/", (_req, res) => {
--- todo-server/src/todos/routes.ts
+++ todo-server/src/todos/routes.ts
@@ -5,7 +5,7 @@ const todosRouter = express.Router();
 
 todosRouter.get("/", async (_req, res) => {
   try {
-    let todos_list = await list_todo();
+    const todos_list = await list_todo();
     res.send(todos_list);
   } catch (err) {
     res.status(500).send("INTERNAL_SERVICE_ERROR")
