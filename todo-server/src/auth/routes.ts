1a315c3d93255f84792f511a37af53e9551279f8
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 18:00:02 -0400

    add login route and delete auth-middleware file

---
+++ todo-server/src/auth/routes.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { LoginFail } from "../utils/error";
+import jwt from "jsonwebtoken";
+import { serialize } from "cookie";
+
+const loginRouter = express.Router();
+
+loginRouter.post("/", (req, res, next) => {
+  try {
+    let { username, password } = req.body;
+    if (username !== "onebyte" || password !== "123") {
+      throw new LoginFail(null);
+    }
+
+    //TODO: generate actual secret token
+    const token = jwt.sign({
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+      username,
+    }, "elpepe");
+
+    const serializedToken = serialize("authToken", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60 * 24 * 30,
+      path: "/",
+    });
+
+    res.setHeader("Set-Cookie", serializedToken);
+
+    res.json({ succes: true });
+  } catch (err) {
+    next(err);
+  }
+});
