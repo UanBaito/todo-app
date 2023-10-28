21e6f695d1e3d926d3aed84c345c6503d4074a93
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 10:36:43 -0400

    add controllers for todos
    add basic controllers for todos to add, create, and delete

--- todo-server/src/db/db.ts
+++ todo-server/src/db/db.ts
@@ -1,9 +1,11 @@
-import "dotenv/config"
-import knex from "knex"
+import "dotenv/config";
+import knex from "knex";
 
-const _pg = knex({
-  client: 'pg',
+const db = knex({
+  client: "pg",
   connection: {
     connectionString: process.env.DB_URL,
-  }
+  },
 });
+
+export default db;
---
+++ todo-server/src/todos/model.ts
@@ -0,0 +1,25 @@
+import { Todo } from "knex/types/tables";
+import db from "../db/db.ts";
+
+export async function create_todo(name: string) {
+  console.log(`->> HANDLER - create_todo`)
+  const todo = await db<Todo>("todos").insert({ name: name }).returning("*");
+  return todo[0];
+}
+
+export async function list_todo() {
+  console.log(`->> HANDLER - list_todo`)
+  const todos = await db.select().from("todos")
+  return todos
+}
+
+export async function delete_todo(id: number) {
+  console.log(`->> HANDLER - delete_todo`)
+  const todo = await db<Todo>("todos").where("id", id).del().returning("*")
+  //TODO: throw error if id was not found
+  // if (todo.length === 0) {
+  //   return 
+  // }
+  return todo[0]
+}
+
