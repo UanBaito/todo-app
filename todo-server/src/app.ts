35fcea7497426a7afeef8e312f17cf9b6dd47e51
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 10:43:56 -0400

    nest todos router under /api

--- todo-server/src/app.ts
+++ todo-server/src/app.ts
@@ -4,13 +4,14 @@ import todosRouter from "./todos/routes.ts"
 const app = express()
 const port = 3000
 
-app.use("/todos", todosRouter)
+app.use("api/todos", todosRouter)
 
 app.get("/", (_req, res) => {
   res.send("Hello world")
 })
 
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
+
+
