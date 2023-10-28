e0b0d8e8e385dca04a6f4e3398c77b20d44680b8
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 17:40:28 -0400

    add cookie logger and parser

--- todo-server/src/app.ts
+++ todo-server/src/app.ts
@@ -1,8 +1,9 @@
-import express, { ErrorRequestHandler } from "express";
+import express from "express";
 import todosRouter from "./todos/routes.ts";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { TodoDeleteFailIdNotFound } from "./utils/error.ts";
+import cookieParser from "cookie-parser"
+import { errorHandler, logger } from "./middleware.ts";
 
 export const app = express();
 const port = 3000;
@@ -15,23 +16,17 @@ app.use((_req, _res, next)=> {
   next()
 })
 
+app.use(cookieParser())
 app.use(cors());
 app.use(bodyParser.json());
+app.use(logger)
+
+
 app.use("/api/todos", todosRouter);
 
 app.get("/", (_req, res) => {
   res.send("Hello world");
 });
-
-const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  console.log(`->> ERROR-HANDLER - ${err.constructor.name} - ${JSON.stringify(err.body)}`)
-
-  if (err instanceof TodoDeleteFailIdNotFound) {
-    res.status(400).send(
-      { "error": "INVALID_PARAMS" },
-    );
-  }
-};
 
 app.use(errorHandler);
 
