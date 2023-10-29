9b804f14f68589f4a2b07649b855ae5df94b85af
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 20:09:35 -0400

    moved login test to tope of test suite

--- todo-server/tests/request.test.ts
+++ todo-server/tests/request.test.ts
@@ -1,6 +1,15 @@
 import request from "supertest";
 
 let hc = request.agent("http://localhost:3000");
+
+test("login", async () => {
+  let response = await hc.post("/api/login").send({"username": "onebyte", "password": "123"});
+  if (response.error) {
+    console.log(response.body);
+  }
+  expect(response.statusCode).toBe(200);
+});
+
 test("list_todos", async () => {
   let response = await hc.get("/api/todos").expect(200);
   if (response.error) {
@@ -25,8 +34,6 @@ test("delete_todo", async () => {
   expect(response.statusCode).toBe(200);
 });
 
-
-
 test("empty-credentials", async () => {
   let response = await hc.post("/api/login");
   if (response.error) {
@@ -43,10 +50,3 @@ test("wrong-credentials", async () => {
   expect(response.statusCode).toBe(403);
 });
 
-test("login", async () => {
-  let response = await hc.post("/api/login").send({"username": "onebyte", "password": "123"});
-  if (response.error) {
-    console.log(response.body);
-  }
-  expect(response.statusCode).toBe(200);
-});
