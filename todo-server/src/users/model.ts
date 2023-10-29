2cac620faf9ef01686f8da8c5edb2dd6de68f2b7
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 22:08:46 -0400

    update userModel with method to retrieve pwd hash

---
+++ todo-server/src/users/model.ts
@@ -0,0 +1,14 @@
+// import { User } from "knex/types/tables"
+import db from "../db/db.ts";
+
+export class UserModel {
+  async findUserByName(name: string) {
+    //TODO: make username unique on db, or use email instead
+    const user = await db("users").first("name", "id", "created_at").where("name", name);
+    return user;
+  }
+  async getUserPwdHash(id: string) {
+    const pwdHash = await db("users").first("pwd_hash").where("id", id)
+    return pwdHash
+  }
+}
