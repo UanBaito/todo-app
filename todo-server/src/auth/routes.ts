16a4706a32e5f63d0535f993e4f094a33ad474c1
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-28 20:56:27 -0400

    add logout route

--- todo-server/src/auth/routes.ts
+++ todo-server/src/auth/routes.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { LoginFail } from "../utils/error.ts";
+import {
+  AuthFailTokenWrongFormat,
+  LoginFail,
+} from "../utils/error.ts";
 import jwt from "jsonwebtoken";
 import { serialize } from "cookie";
 
 export const loginRouter = express.Router();
 
 loginRouter.post("/login", (req, res, next) => {
-  console.log(`->> HANDLER - login`)
+  console.log(`->> HANDLER - login`);
   try {
     let { username, password } = req.body;
     if (username !== "onebyte" || password !== "123") {
@@ -32,5 +35,29 @@ loginRouter.post("/login", (req, res, next) => {
     res.json({ succes: true });
   } catch (err) {
     next(err);
+  }
+});
+
+loginRouter.post("/logout", (req, res, next) => {
+  let { authToken } = req.cookies;
+  try {
+    if (!authToken) {
+      res.send("USER WAS NOT LOGGED IN");
+    }
+    jwt.verify(authToken, "secret");
+
+    const serializedToken = serialize("authToken", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 0,
+      path: "/",
+    });
+
+    res.setHeader("Set-Cookie", serializedToken);
+
+    res.send({ "succes": true });
+  } catch (err) {
+    next(new AuthFailTokenWrongFormat(null));
   }
 });
