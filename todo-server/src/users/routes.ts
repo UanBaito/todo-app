7cb20a58e300197fe703698a37915c46066320d4
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-29 13:16:18 -0400

    update userDelete route to require context id to match request id

--- todo-server/src/users/routes.ts
+++ todo-server/src/users/routes.ts
@@ -2,22 +2,30 @@ import express from "express";
 import { UserModel } from "./model.ts";
 
 export const userRouter = express.Router();
-const userModel = new UserModel()
+const userModel = new UserModel();
 
 //TODO: make this use context
 userRouter.delete("/:id", async (req, res, next) => {
+  const { ctx } = res.locals;
+  const { id } = req.params;
+  console.log(`->> HANDLER - delete_user`);
   try {
-    console.log(`->> HANDLER - delete_user`);
-    console.log(JSON.stringify(res.locals))
-    const { id } = req.params;
+    if (!ctx) {
+      //TODO: Create new error for no context
+      throw new Error();
+    }
     if (!id) {
       //TODO: make new error for userdelete fail id not provided
       throw new Error();
     }
-    const user = await userModel.deleteUser(id)
+    if (ctx.user.id !== id) {
+      //TODO: Create new error for unauthorized action for context
+      throw new Error()
+    }
+    const user = await userModel.deleteUser(id);
 
     //TODO: Check if i am sending all objects as jsons
-    res.json(user)
+    res.json(user);
   } catch (err) {
     next(err);
   }
