4f939b5f26aef6bb79692ead87883e9aa913157d
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-29 12:12:03 -0400

    implement create user method on userModel

--- todo-server/src/users/model.ts
+++ todo-server/src/users/model.ts
@@ -1,14 +1,23 @@
 // import { User } from "knex/types/tables"
-import { User } from "../utils/interfaces.ts";
+import { User, UserForCreate } from "../utils/interfaces.ts";
 import db from "../db/db.ts";
+import bcrypt from "bcrypt";
 
 export class UserModel {
   async findUserByName(name: string) {
-    const user = await db<User>("users").first("name", "id", "created_at").where("name", name);
+    const user = await db<User>("users").first("name", "id", "created_at")
+      .where("name", name);
     return user;
   }
   async getUserPwdHash(id: string) {
-    const pwdHash = await db<User>("users").first("pwd_hash").where("id", id)
-    return pwdHash
+    const pwdHash = await db<User>("users").first("pwd_hash").where("id", id);
+    return pwdHash;
+  }
+  async CreateUser(newUserInfo: UserForCreate) {
+    const { name, pwd } = newUserInfo;
+    const pwd_hash = await bcrypt.hash(pwd, 12);
+    const user = await db<User>("users").insert({ name, pwd_hash }).returning(["id", "name", "created_at"]);
+    console.log(user);
+    return user;
   }
 }
