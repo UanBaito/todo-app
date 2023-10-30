c426b6e888a312c1b655a268a4aaa8140b0921d0
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-30 10:41:15 -0400

    add getuserdata hook to dashboard

---
+++ todo-client/src/Dashboard.tsx
@@ -0,0 +1,13 @@
+import getUserData from "./hooks/getUserData";
+
+export default function Dashboard() {
+  const queryState = getUserData();
+
+  if (queryState.isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (queryState.isError) {
+    return <div>error</div>;
+  }
+  return <div>{queryState.data}</div>;
+}
