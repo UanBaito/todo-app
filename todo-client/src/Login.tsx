08f27f7fe09c4cb4e6a4c59672b99eb6e31a6b7c
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-30 10:07:47 -0400

    deleted some unused files and created new ones for pages

---
+++ todo-client/src/Login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <form>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="password" />
+    </form>
+  );
+}
--- todo-client/src/main.tsx
+++ todo-client/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./Login";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<div>Hello world</div>} path="/"></Route>
+      <Route element={<Login />} path="/login" />
+    </>,
+  ),
+);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
-)
+);
