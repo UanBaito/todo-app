4e4206a4b68d3a5a06e2175b3ee0e0ddf2fefc9a
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-30 19:44:13 -0400

    create todoslist component

---
+++ todo-client/src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+
+export default function () {
+  const todoListQuery = useQuery({
+    queryKey: ["todosList"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:3000/api/todos");
+      if (!res.ok) {
+        throw new Error();
+      }
+      const result = await res.json();
+      return result;
+    },
+  });
+  if (todoListQuery.isLoading) {
+    return <div>Loading</div>;
+  }
+  if (todoListQuery.isError) {
+    return <div>error</div>;
+  }
+  console.log(todoListQuery.data);
+  return <div>loaded</div>;
+}
