8b327cf257741170f7ff0792af8d1c1434abbb78
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-30 10:39:45 -0400

    implement get user data functionality to hook

---
+++ todo-client/src/hooks/getUserData.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+export default function getUserData() {
+  const userDataQuery = useQuery({
+    queryKey: ["userData"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:3000/api/users/");
+      if (!res.ok) {
+        console.log("error fetching user");
+        throw new Error();
+      }
+      const data = res.json();
+      return data;
+    },
+  });
+  return userDataQuery
+}
