7923e3408133a306b10f86fbdb5635a9a1633bac
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-29 21:03:25 -0400

    add new getTodoById method on todos model

--- todo-server/src/todos/model.ts
+++ todo-server/src/todos/model.ts
@@ -3,26 +3,31 @@ import db from "../db/db.ts";
 import { TodoDeleteFailIdNotFound } from "../utils/error.ts";
 
 export class TodosModel {
- async createTodo(name: string, cid: string) {
-  const todo = await db<Todo>("todos").insert({ name, cid }).returning("*");
-  return todo[0];
-}
+  async createTodo(name: string, cid: string) {
+    const todo = await db<Todo>("todos").insert({ name, cid }).returning("*");
+    return todo[0];
+  }
 
- async listTodoByCid(cid: string) {
-  const todos = await db<Todo>("todos").select().where("cid", cid)
-  return todos
-}
+  async listTodoByCid(cid: string) {
+    const todos = await db<Todo>("todos").select().where("cid", cid);
+    return todos;
+  }
+  async getTodoById(id: number) {
+    const todo = await db<Todo>("todos").first().where("id", id);
+    if (!todo) {
+      //TODO: make new todogetfailidnotfound error
+      throw new Error();
+    }
+    return todo
+  }
 
- async deleteTodo(id: number) {
-  const todo = await db<Todo>("todos").where("id", id).del().returning("*")
-   if (todo.length === 0) {
-     throw new TodoDeleteFailIdNotFound({
-      id: id
-    })
-   }
-  return todo[0]
+  async deleteTodo(id: number) {
+    const todo = await db<Todo>("todos").where("id", id).del().returning("*");
+    if (todo.length === 0) {
+      throw new TodoDeleteFailIdNotFound({
+        id: id,
+      });
+    }
+    return todo[0];
+  }
 }
-  
-}
-
-
