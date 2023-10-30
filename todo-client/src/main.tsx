7023380cd16f899b9c58e42703b3d42f431e28f3
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-30 10:40:11 -0400

    add dashboard page to router and queryclient provider

--- todo-client/src/main.tsx
+++ todo-client/src/main.tsx
@@ -7,17 +7,24 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Login from "./Login";
+import Dashboard from "./Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route element={<div>Hello world</div>} path="/"></Route>
       <Route element={<Login />} path="/login" />
+      <Route element={<Dashboard />} path="/dashboard" />
     </>,
   ),
 );
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
