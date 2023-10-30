76cb9d22049673738a5c4a1de6e2c92bb8693786
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-30 10:23:32 -0400

    add new getUserInfo route

--- todo-server/src/users/routes.ts
+++ todo-server/src/users/routes.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import { UserModel } from "./model.ts";
-import { AuthFailContextDoesntMatchRequest, AuthFailNoContext, UserDeleteFailIdNotFound } from "../utils/error.ts";
+import {
+  AuthFailContextDoesntMatchRequest,
+  AuthFailNoContext,
+  EmptyForm,
+  UserDeleteFailIdNotFound,
+} from "../utils/error.ts";
 import { Ctx } from "../utils/interfaces.ts";
 import { serialize } from "cookie";
 
@@ -8,11 +13,11 @@ export const userRouter = express.Router();
 const userModel = new UserModel();
 
 userRouter.delete("/:id", async (req, res, next) => {
-  const { userInfo } = (res.locals as Ctx)
+  const { userInfo } = res.locals as Ctx;
   const { id } = req.params;
   console.log(`->> HANDLER - delete_user`);
   try {
-    if (!userInfo ) {
+    if (!userInfo) {
       throw new AuthFailNoContext(null);
     }
     if (userInfo.id !== id) {
@@ -33,6 +38,26 @@ userRouter.delete("/:id", async (req, res, next) => {
     res.setHeader("Set-Cookie", serializedToken);
     //TODO: Check if i am sending all objects as jsons
     res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+userRouter.get("/:id", async (req, res, next) => {
+  console.log("->> HANDLER - get_user");
+  const { userInfo } = res.locals as Ctx;
+  const { id } = req.params;
+  try {
+    if (!userInfo) {
+      throw new AuthFailNoContext(null);
+    }
+    if (!id) {
+      throw new EmptyForm(null);
+    }
+    if (userInfo.id !== id) {
+      throw new AuthFailContextDoesntMatchRequest(null);
+    }
+    res.send(userInfo);
   } catch (err) {
     next(err);
   }
