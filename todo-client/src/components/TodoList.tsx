6d84a2fbbb841b1e24b149e102291c6d26bdfe99
UanBaito <fernandoanibalreyes@gmail.com>
2023-10-31 21:06:25 -0400

    delete edit todo functionality

--- todo-client/src/components/TodoList.tsx
+++ todo-client/src/components/TodoList.tsx
@@ -30,35 +30,35 @@ export default function TodoList() {
 }
 
 export function TodoItem({ todo }: { todo: any }) {
-  const [isEditing, setIsEditing] = useState(false);
+  // const [isEditing, setIsEditing] = useState(false);
   const [todoName, setTodoName] = useState(todo.name);
   const [isCompleted, setIsCompleted] = useState<boolean>(todo.isCompleted);
   const queryClient = useQueryClient();
 
-  const updateTodoMutation = useMutation({
-    mutationFn: async () => {
-      const res = await fetch("http://localhost:3000/api/todos", {
-        method: "PUT",
-        credentials: "include",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(
-          {
-            name: todoName,
-            id: todo.id,
-            isCompleted: todo.isCompleted,
-          },
-        ),
-      });
-      if (!res.ok) {
-        throw new Error();
-      }
-      const result = await res.json();
-      return result;
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["todosList"] });
-    },
-  });
+  // const updateTodoMutation = useMutation({
+  //   mutationFn: async () => {
+  //     const res = await fetch("http://localhost:3000/api/todos", {
+  //       method: "PUT",
+  //       credentials: "include",
+  //       headers: { "Content-type": "application/json" },
+  //       body: JSON.stringify(
+  //         {
+  //           name: todoName,
+  //           id: todo.id,
+  //           isCompleted: todo.isCompleted,
+  //         },
+  //       ),
+  //     });
+  //     if (!res.ok) {
+  //       throw new Error();
+  //     }
+  //     const result = await res.json();
+  //     return result;
+  //   },
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({ queryKey: ["todosList"] });
+  //   },
+  // });
 
   const toggleCompleteMutation = useMutation({
     mutationFn: async () => {
@@ -85,72 +85,21 @@ export function TodoItem({ todo }: { todo: any }) {
 
   return (
     <>
-      {isEditing
-        ? (
-          <li>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                setIsEditing(false);
-                updateTodoMutation.mutate();
-              }}
-            >
-              <input
-                className={styles.name_input}
-                name="todo name"
-                type="text"
-                value={todoName}
-                onChange={(e) => {
-                  setTodoName(e.target.value);
-                }}
-              />
-              <button>submit</button>
-            </form>
-            <label htmlFor={`edit_checkbox_${todo.id}`}>
-              <FaXmark></FaXmark>
-            </label>
-            <input
-              id={`edit_checkbox_${todo.id}`}
-              className={styles.edit_checkbox}
-              name="edit"
-              type="checkbox"
-              checked={isEditing}
-              onChange={() => {
-                setIsEditing((prevState) => !prevState);
-              }}
-            />
-          </li>
-        )
-        : (
-          <li>
-            <input
-              className={styles.completed_checkbox}
-              name="completed"
-              type="checkbox"
-              checked={isCompleted}
-              onChange={() => {
-                setIsCompleted((prevState) => !prevState);
-                toggleCompleteMutation.mutate();
-              }}
-            />
-            <h2>
-              {todo.name}
-            </h2>
-            <label htmlFor={`edit_checkbox_${todo.id}`}>
-              <FaPencil></FaPencil>
-            </label>
-            <input
-              id={`edit_checkbox_${todo.id}`}
-              className={styles.edit_checkbox}
-              name="edit"
-              type="checkbox"
-              checked={isEditing}
-              onChange={() => {
-                setIsEditing((prevState) => !prevState);
-              }}
-            />
-          </li>
-        )}
+      <li>
+        <input
+          className={styles.completed_checkbox}
+          name="completed"
+          type="checkbox"
+          checked={isCompleted}
+          onChange={() => {
+            setIsCompleted((prevState) => !prevState);
+            toggleCompleteMutation.mutate();
+          }}
+        />
+        <h2>
+          {todo.name}
+        </h2>
+      </li>
     </>
   );
 }
