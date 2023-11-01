39e79cbccd2fc4538f7094e92a49b3ba8c61c438
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-01 09:40:54 -0400

    remove unused import

--- todo-client/src/components/TodoList.tsx
+++ todo-client/src/components/TodoList.tsx
@@ -1,7 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import styles from "./styles/TodoList.module.scss";
-import { FaPencil, FaXmark } from "react-icons/fa6";
+import { FaXmark } from "react-icons/fa6";
 
 export default function TodoList() {
   const todoListQuery = useQuery({
