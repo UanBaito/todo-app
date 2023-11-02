54baf943734f37b8f5ad2958ad60e3c0d292539a
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-02 14:38:11 -0400

    implement RemoveCompletedTodos functionality

---
+++ todo-client/src/components/RemoveCompletedTodos.tsx
@@ -0,0 +1,31 @@
+import { useMutation } from "@tanstack/react-query";
+import styles from "./styles/RemoveCompletedTodos.module.scss";
+
+export default function RemoveCompletedTodos(
+  { existsCompleted }: { existsCompleted: boolean },
+) {
+  const removeCompletedTodosMutation = useMutation({
+    mutationFn: async () => {
+      const res = await fetch("http://localhost:3000/api/todos", {
+        method: "DELETE",
+        credentials: "include",
+      });
+      if (!res.ok) {
+        throw new Error();
+      }
+      const result = await res.json();
+      return result;
+    },
+  });
+
+  return (
+    <button
+      onClick={() => {
+        removeCompletedTodosMutation.mutate();
+      }}
+      disabled={!existsCompleted}
+      className={styles.button}
+    >
+    </button>
+  );
+}
