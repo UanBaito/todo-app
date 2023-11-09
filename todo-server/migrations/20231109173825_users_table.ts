d36392483d76a4f15208509d44ab68956e7a997b
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-09 13:54:30 -0400

    create users_table migration

---
+++ todo-server/migrations/20231109173825_users_table.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', table => {
+    table.uuid("id").primary().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.string("name", 40).notNullable()
+    table.date('created_at').defaultTo(knex.raw('now()'))
+    table.string('pwd_hash').notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  knex.schema.dropTable('users')
+}
+
