bc598e1f81ee13fd109a8b9ddfa9d259ece4e4f5
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-09 14:06:26 -0400

    fix uuid_generate_v4() function doesnt exist error

--- todo-server/migrations/20231109173825_users_table.ts
+++ todo-server/migrations/20231109173825_users_table.ts
@@ -1,17 +1,15 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('users', table => {
-    table.uuid("id").primary().defaultTo(knex.raw('uuid_generate_v4()'))
-    table.string("name", 40).notNullable()
-    table.date('created_at').defaultTo(knex.raw('now()'))
-    table.string('pwd_hash').notNullable()
-  })
+  return await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable("users", (table) => {
+      table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+      table.string("name", 40).notNullable();
+      table.date("created_at").defaultTo(knex.raw("now()"));
+      table.string("pwd_hash").notNullable();
+    });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-  knex.schema.dropTable('users')
+  knex.schema.dropTable("users");
 }
-
