4419f4070ffbf17f3a5d8db673bf95ca3ba732f8
UanBaito <fernandoanibalreyes@gmail.com>
2023-11-11 13:47:52 -0400

    update buttons to navigate to login/register page onclick

--- todo-client/src/Home.tsx
+++ todo-client/src/Home.tsx
@@ -1,11 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./styles/Home.module.scss";
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <main>
       <section className={styles.container}>
         <h1>TO-DO APP</h1>
-        <button className={styles.login}>Login</button>
-        <button className={styles.register}>Register</button>
+        <button
+          className={styles.login}
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Login
+        </button>
+        <button
+          className={styles.register}
+          onClick={() => {
+            navigate("/register");
+          }}
+        >
+          Register
+        </button>
       </section>
     </main>
   );
